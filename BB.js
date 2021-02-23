4583792fdac62c583a2593d52657ab4750255b22
redgpcmffld <76928178+redgpcmffld@users.noreply.github.com>
2021-02-23 16:55:59 +0900

    Update BB.js

--- BB.js
+++ BB.js
@@ -8,3 +8,9 @@ ctx.rect(230,470,50,15);
 ctx.fillstyle = "rgb(0,0,0)";
 ctx.fill();
 ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(255,460,5,0,90,false);
+ctx.fillstyle = "black";
+ctx.fill();
+ctx.closePath();
