3afe82aea3d781cbc083d4e5f11e7417d1a3260d
redgpcmffld <76928178+redgpcmffld@users.noreply.github.com>
2021-02-23 20:22:47 +0900

    Update BB.js

--- BB.js
+++ BB.js
@@ -2,14 +2,33 @@
 
 let canvas = document.getElementById('BB');
 let ctx = canvas.getContext("2d");
+let cw = canvas.width;
+let ch = canvas.height;
 let x = canvas.width/2;
 let y = canvas.height-40;
 let dx = 2;
 let dy = -2;
 let ballRadius = 5;
-let bar = 10;
+let barHeight = 10;
+let barWidth = 50;
+let barX = (canvas.width-barWidth)/2;
+let barY = canvas.height-barHeight-10;
+let ground = canvas.height-ballRadius-barHeight-10;
+let screenLog = document.querySelector('#screen-log');
+document.addEventListener('mousemove', logKey);
 
-
+function logKey(e) {
+    screenLog.innerText = `
+      Screen X/Y: ${e.screenX}, ${e.screenY}
+      Client X/Y: ${e.clientX}, ${e.clientY}`;
+  }
+  function checkClickMap(e) {
+    if (e.screenX < 50) doRedButton();
+    if (50 <= e.screenX && e.screenX < 100) doYellowButton();
+    if (e.screenX >= 100) doRedButton();
+  }
+console.log(screenY);
+console.log("Hello world")
 function drawball(){
     ctx.beginPath();
     ctx.arc(x,y,ballRadius,0,Math.PI*2,false);
@@ -19,7 +38,7 @@ function drawball(){
 }
 function drawbar(){
     ctx.beginPath();
-    ctx.rect(canvas.width/2-25,canvas.height-30,50,15);
+    ctx.rect(screenX,screenY,barWidth,barHeight);
     ctx.fillstyle = "black";
     ctx.fill();
     ctx.closePath();
@@ -28,7 +47,7 @@ function draw(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
     drawball();
     drawbar();
-    if(y+dy>canvas.height-ballRadius-30 || y+dy < ballRadius){
+    if(y+dy>ground || y+dy < ballRadius){
         dy=-dy;
     }
     if(x+dx>canvas.width-ballRadius || x+dx < ballRadius){
