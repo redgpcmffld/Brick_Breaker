82e0d2791fa31c504bb8a2d7120f647bb1cbf815
redgpcmffld <76928178+redgpcmffld@users.noreply.github.com>
2021-02-23 17:11:04 +0900

    Update BB.js

--- BB.js
+++ BB.js
@@ -2,15 +2,24 @@
 
 let canvas = document.getElementById('BB');
 let ctx = canvas.getContext("2d");
+let x = canvas.width/2;
+let y = canvas.height-40;
+let dx = 2;
+let dy = -2;
 
-ctx.beginPath();
-ctx.rect(230,470,50,15);
-ctx.fillstyle = "rgb(0,0,0)";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.arc(255,460,5,0,90,false);
-ctx.fillstyle = "black";
-ctx.fill();
-ctx.closePath();
+function draw(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x,y,5,0,90,false);
+    ctx.fillstyle = "black";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+    ctx.beginPath();
+    ctx.rect(canvas.width/2-25,canvas.height-30,50,15);
+    ctx.fillstyle = "rgb(0,0,0)";
+    ctx.fill();
+    ctx.closePath();
+}
+setInterval(draw,10);
