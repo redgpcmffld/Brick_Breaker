077e49b626f7f3c85187e9da41f051e340e25d6e
redgpcmffld <76928178+redgpcmffld@users.noreply.github.com>
2021-02-23 17:41:18 +0900

    Update BB.js

--- BB.js
+++ BB.js
@@ -6,20 +6,34 @@ let x = canvas.width/2;
 let y = canvas.height-40;
 let dx = 2;
 let dy = -2;
+let ballRadius = 5;
 
-function draw(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+
+function drawball(){
     ctx.beginPath();
-    ctx.arc(x,y,5,0,90,false);
+    ctx.arc(x,y,ballRadius,0,Math.PI*2,false);
     ctx.fillstyle = "black";
     ctx.fill();
     ctx.closePath();
-    x += dx;
-    y += dy;
+}
+function drawbar(){
     ctx.beginPath();
     ctx.rect(canvas.width/2-25,canvas.height-30,50,15);
-    ctx.fillstyle = "rgb(0,0,0)";
+    ctx.fillstyle = "black";
     ctx.fill();
     ctx.closePath();
 }
+function draw(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    drawball();
+    drawbar();
+    if(y+dy>canvas.height-30 || y+dy <0){
+    dy=-dy;
+    }
+    // x += dx;
+    y += dy; 
+}
+
+
 setInterval(draw,10);
+
